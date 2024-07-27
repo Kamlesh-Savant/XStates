1dd9d3f2c818868c84a21ba984615103653af147
Albert <albert.but@hotmail.com>
2024-07-27 20:55:59 +0530

    added some functionality in dropdowns

--- xstates/src/App.js
+++ xstates/src/App.js
@@ -4,11 +4,20 @@ import React, { useEffect, useState } from 'react'
 function App() {
 
   const [countryList, setCountryList] = useState([]);
+  const [stateList, setStateList] = useState([]);
+  const [cityList, setCityList] = useState([]);
   const [selectedCountry, setSelectedCountry] = useState('');
+  const [selectedState, setSelectedState] = useState('');
+  const [selectedCity, setSelectedCity] = useState('');
 
-  const fetchCountry = async () => {
+  const fetchCountry = async (e) => {
     try {
       
+      // if(e.target.value === "select"){
+      //   selectedCountry('');
+      //   selectedState('');
+      //   return;
+      // }
       let response = await fetch('https://crio-location-selector.onrender.com/countries');
       let data = await response.json();
 
@@ -19,13 +28,58 @@ function App() {
     }
   }
 
-  const fetchState = async () => {
+  const fetchState = async (e) => {
     try {
-      
-      let response = await fetch('https://crio-location-selector.onrender.com/country=${selectedCountry}/states');
-      let data = await response.json();
+      if(e.target.value === "select"){
+        setStateList([]);
+        setCityList([]);
+        setSelectedCountry('');
+        setSelectedState('');
+        return;
+      }
 
-      setCountryList(data);
+      setSelectedCountry(e.target.value);
+      let response = await fetch(`https://crio-location-selector.onrender.com/country=${e.target.value}/states`);
+      let data = await response.json();
+      
+      // console.log(data);
+      setStateList(data);
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const fetchCity = async (e) => {
+    try {
+
+      if(e.target.value === "select"){
+        setCityList([]);
+        setSelectedState('');
+
+        return;
+      }
+
+      setSelectedState(e.target.value);
+      let response = await fetch(`https://crio-location-selector.onrender.com/country=${selectedCountry}/state=${e.target.value}/cities`);
+      let data = await response.json();
+      
+      // console.log(data);
+      setCityList(data);
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const displayLocation = async (e) => {
+    try {
+
+      if(e.target.value === "select"){
+        setSelectedCity('');
+        return;
+      }
+      setSelectedCity(e.target.value);
 
     } catch (error) {
       console.log(error);
@@ -38,16 +92,38 @@ function App() {
     fetchCountry();
   },[]);
 
+  
+
   return (
     <div className="App">
       <h1>Select Location</h1>
-      <div>
+      <div className='container'>
         <select onChange={fetchState}>
           <option value="select">Select Country</option>
         {countryList.map((countryName)=>{
           return <option value={countryName}>{countryName}</option>
         })}
         </select>
+
+        <select onChange={fetchCity} disabled={selectedCountry === "" ? true : false}>
+          <option value="select">Select State</option>
+        {stateList.map((stateName)=>{
+          return <option value={stateName}>{stateName}</option>
+        })}
+        </select>
+
+        <select disabled={selectedState === "" ? true : false} onChange={displayLocation}>
+          <option value="select">Select City</option>
+        {cityList.map((cityName)=>{
+          return <option value={cityName}>{cityName}</option>
+        })}
+        </select>
+
+      </div>
+      <div className='location'>
+        {selectedCity && 
+          <p>You Selected <strong>{selectedCity}</strong>, {selectedState}, {selectedCountry}</p>
+        }
       </div>
     </div>
   );
